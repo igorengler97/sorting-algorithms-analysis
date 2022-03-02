c0a872d2aa77f30a9d5c361f3b9f3b8851a82b57
Igor Engler <igorengler97@hotmail.com>
2022-03-02 19:18:34 -0300

    fixed insertion sort

--- sort.cpp
+++ sort.cpp
@@ -9,7 +9,7 @@ void insertion_sort(std::vector<int>& vec)
 		value = vec[i];
 		j = i - 1;
 
-		while (j >= 0 && vec[i] > value)
+		while (j >= 0 && vec[j] > value)
 		{
 			vec[j + 1] = vec[j];
 			j--;
